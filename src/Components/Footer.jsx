2e332baf59c6b2d998f38c7276a750a60bde8db2
Bikash <bikash.k3044@gmail.com>
2023-11-23 15:46:27 +0530

    first commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
     <section className="newsletter">
     <div className="container">
       <div className="row">
-        <div className="col-lg-6 col-md-6 col-6 mb-5 footer-image">
+        <div className="col-lg-6 col-md-6 col-0 footer-image">
           <img className="news-iphone" src="Images/iPhone.png" alt=""/>
         </div>
-        <div className="col-lg-6 col-md-6 col-6 mb-5">
+        <div className="col-lg-6 col-md-6 col-12 mb-5">
           <h2 className="news-heading">Download Miami Social <br/> Media App Today!</h2>
           <p className="news-para">The perfect Social Networking App for you.</p>
           <div className="mt-4">
--- src/Components/Section3.jsx
+++ src/Components/Section3.jsx
@@ -6,13 +6,13 @@ export default function Section3() {
     <section id="third">
     <div className="container">
       <div className="row">
-        <div className="col-lg-6 col-md-6 col-6 third-iphone-div">
+        <div className="col-lg-6 col-md-6 col-12 third-iphone-div">
           <h2 className="third-heading">Share your every <br/> BIG moment!</h2>
           <p className="third-para">The Perfect app to share your every memorable <br/> moment to your friends and around
             the world</p>
           <button className="btn main-btn third-button" type="submit">Download Now</button>
         </div>
-        <div className="col-lg-6 col-md-6 col-6 image"><img
+        <div className="col-lg-6 col-md-6 col-12 image"><img
             className="iphone-image" src="Images/iPhone.png" alt=''/></div>
       </div>
     </div>
